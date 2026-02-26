9ce49f81b5c993a3d1d8759e7e548b9be0c7c363
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-02-26 15:45:11 +0530

    js

--- Javascript/ex.js
+++ Javascript/ex.js
@@ -1,1 +1,8 @@
-console.log("Hello External js")
+// console.log("Hello External js")
+
+// Bom : browser object model
+
+// alert("pls feild a data..");
+// confirm("Are your sure ?")
+// prompt("5+5 = ?");
+// print()

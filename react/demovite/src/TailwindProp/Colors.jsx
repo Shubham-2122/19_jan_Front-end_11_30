eefbda885d4053a29d4e1cc8f9441fee560755e9
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-04-16 15:56:04 +0530

    react

---
+++ react/demovite/src/TailwindProp/Colors.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Colors() {
+  return (
+    <div>
+        <h1 className='text-4xl bg-amber-500 text-blue-400'>Hello this Color property</h1>
+        <h1 className='text-4xl bg-green-600'>Hello this Color property</h1>
+        <h1 className='text-4xl bg-gray-500'>Hello this Color property</h1>
+        <h1 className='text-4xl bg-sky-700'>Hello this Color property</h1>
+        <h1 className='text-4xl bg-red-500'>Hello this Color property</h1>
+        <h1 className='text-4xl bg-lime-600'>Hello this Color property</h1>
+
+                <h1 className='text-4xl text-amber-500 '>Hello this Color property</h1>
+        <h1 className='text-4xl text-green-600'>Hello this Color property</h1>
+        <h1 className='text-4xl text-gray-500'>Hello this Color property</h1>
+        <h1 className='text-4xl text-sky-700'>Hello this Color property</h1>
+        <h1 className='text-4xl text-red-500'>Hello this Color property</h1>
+        <h1 className='text-4xl text-lime-600'>Hello this Color property</h1>
+        <h1 className='text-4xl'>Hello this Color property</h1>
+    </div>
+  )
+}
+
+export default Colors
---
+++ react/demovite/src/TailwindProp/Fonts.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Fonts() {
+    return (
+        <div>
+            <h1 className='font-sans text-xl'>Hello this data here</h1>
+            <h1 className='font-serif underline text-2xl  decoration-dotted text-right'>Hello this data here</h1>
+            <h1 className='font-mono overline capitalize decoration-double text-3xl'>Hello this data here</h1>
+            <h1 className='text-4xl line-through decoration-amber-600 text-center'>Hello this data here</h1>
+            <h1 className=' font-bold '>Hello this data here</h1>
+            <h1 className='text-6xl uppercase underline decoration-2'>Hello this data here</h1>
+            <br />
+            <br />
+            <div className='bg-amber-500 p-11 m-8'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt harum velit, molestias tempore consequuntur rem eos in reprehenderit explicabo excepturi error facere at blanditiis odio debitis? Quod fuga amet exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eum veritatis magnam. Reiciendis dicta eos provident facilis qui, maiores corporis deleniti aperiam iure alias debitis? Aspernatur perspiciatis similique veritatis excepturi.
+            </div>
+            <div className='bg-red-500 pt-6 mt-7 ml-6 pb-6 pl-9 pr-10'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt harum velit, molestias tempore consequuntur rem eos in reprehenderit explicabo excepturi error facere at blanditiis odio debitis? Quod fuga amet exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eum veritatis magnam. Reiciendis dicta eos provident facilis qui, maiores corporis deleniti aperiam iure alias debitis? Aspernatur perspiciatis similique veritatis excepturi.
+            </div>
+            <div className='bg-green-500 px-10 py-16 my-7'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt harum velit, molestias tempore consequuntur rem eos in reprehenderit explicabo excepturi error facere at blanditiis odio debitis? Quod fuga amet exercitationem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eum veritatis magnam. Reiciendis dicta eos provident facilis qui, maiores corporis deleniti aperiam iure alias debitis? Aspernatur perspiciatis similique veritatis excepturi.
+            </div>
+        </div>
+    )
+}
+
+export default Fonts
